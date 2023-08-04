18a5152695268eaa72f37aada1b83deb2c746a84
Fei Liang <njliangfei@gmail.com>
2023-08-04 12:36:49 +0800

    japanese song

--- tools/typesetting.py
+++ tools/typesetting.py
@@ -13,11 +13,13 @@ def format_md_file(file_path):
     content = content.replace('”', '」')
     content = content.replace(',', '，')
     content = content.replace('?', '？')
+    content = content.replace(':', '：')
     print(content)
     with open(file_path, 'w') as f:
         f.write(content)
 
 
 if __name__ == '__main__':
-    file_path = os.path.join(os.getcwd(), '_posts/2023-08-04-washoku-nihonryouri.md')
+    file_path = os.path.join(
+        os.getcwd(), '_posts/2023-08-04-learn-japanese-by-song.md')
     format_md_file(file_path)
