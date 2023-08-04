721f192024589c843b172ac58ee19a59bbec179c
Fei Liang <njliangfei@gmail.com>
2023-08-04 11:10:19 +0800

    add washoku

---
+++ tools/typesetting.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+
+def format_md_file(file_path):
+    print(file_path)
+    with open(file_path, 'r') as f:
+        content = f.read()
+    pattern = r"（(.*?)）"
+    replacement = r"<span class='more'>（\1）</span>"
+    content = re.sub(pattern, replacement, content)
+    content = content.replace('“', '「')
+    content = content.replace('”', '」')
+    content = content.replace(',', '，')
+    content = content.replace('?', '？')
+    print(content)
+    with open(file_path, 'w') as f:
+        f.write(content)
+
+
+if __name__ == '__main__':
+    file_path = os.path.join(os.getcwd(), '_posts/2023-08-04-washoku-nihonryouri.md')
+    format_md_file(file_path)
